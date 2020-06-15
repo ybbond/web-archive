86aa914ed58e8de844484aab3bd2a80ae507b455
Yohanes Bandung Bondowoso <hi@ybbond.dev>
2020-06-15 10:42:10 +0700

    add tags & json

--- index.ts
+++ index.ts
@@ -1,17 +1,29 @@
 import {
   emptyDirSync,
   copySync,
+  readFileStrSync,
   writeFileStrSync,
   walkSync,
 } from "https://deno.land/std/fs/mod.ts";
+
+type JSONContent = {
+  title: string;
+  tags: Array<string>;
+  date: string;
+};
 
 emptyDirSync("./a");
 const content = walkSync("./bookmarks", {
   includeDirs: false,
   skip: [/.DS_Store/],
 });
+
+let jsonObject = JSON.parse(readFileStrSync("./index.json"));
+
 const array = [];
 const result = [];
+const jsonMap: Map<string, JSONContent> = new Map(Object.entries(jsonObject));
+const json: { [key: string]: JSONContent } = {};
 
 for (const entry of content) {
   const [url, date, title] = entry.name.split("-saved---on-");
@@ -37,10 +49,38 @@ for (const entry of sorted) {
   const newUrl = `./a/${url}-saved-${date}.html`;
 
   result.push(
-    `<p><a href="${newUrl}"><span>${
+    `<div class="item" id="${date} ${jsonMap
+      .get(title)
+      ?.tags.map((item) => item)
+      .join(" ")}"><p class="title"><a href="${newUrl}"><span>${
       title.split(".html")[0]
-    }</span></a> saved on <time>${date}</time></p>`
+    }</span></a> saved on <time>${date}</time></p><p class="tags">${jsonMap
+      .get(title)
+      ?.tags.map((item) => `<span id="${item}" class="tagitem">#${item}</span>`)
+      .join("")}
+    </p></div>`
   );
+
+  if (!jsonMap.has(title)) {
+    jsonMap.set(title, { title, tags: [], date });
+  } else {
+    jsonMap.set(
+      title,
+      jsonMap.get(title) || {
+        title,
+        tags: ["hehe"],
+        date: "2020-06-12T00-00-00",
+      }
+    );
+  }
+}
+
+for (const [, entry] of jsonMap.entries()) {
+  json[entry.title] = {
+    title: entry.title,
+    tags: entry.tags,
+    date: entry.date,
+  };
 }
 
 let html = `
@@ -57,6 +97,5 @@ let html = `
 </html>
 `;
 
-console.log(">> html", html);
-
 writeFileStrSync("index.html", html);
+writeFileStrSync("index.json", JSON.stringify(json));
